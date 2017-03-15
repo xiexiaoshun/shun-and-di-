0e35d7b04dfcf7eb877f4dd3f0d9d4b281479e43
zhangshun <q11309925312@qq.com>
2017-03-15 15:53:12 +0800

    9

--- 优橙/js/lou.js
+++ 优橙/js/lou.js
@@ -2,13 +2,28 @@ var myDiv = angular.module("myapp", ['ui.router']);
 myDiv.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	$stateProvider
 		//店铺
-		.state("dian", {
-			url: 'dian',
-			views: {
-				"main": {
-					templateUrl: "myorder.html"
-				}
-			}
-		});
-		//
+		.state("mystore", {
+			url: 'mystore',
+//			views: {
+//				"body": {
+//					templateUrl: "myorder.html"
+//				}
+//			}
+		templateUrl: "mystore.html"
+		})
+		//首页
+		.state("index", {
+			url: 'index',
+			templateUrl: "index.html"
+		})
+		//优橙协助orange_help
+		.state("orange_help", {
+			url: 'orange_help',
+			templateUrl: "orange_help.html"
+		})
+		//优橙协助
+		.state("myorder", {
+			url: 'myorder',
+			templateUrl: "myorder.html"
+		})
 }]);
